43c9ebf5bc3d2ab286ad3e700bb384f2518c9e95
Cristian Aguilar <cristian.aguilar02@gmail.com>
2024-02-28 04:33:05 -0600

    Import/export, funciones de arreglos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// -------------------------------- DESESTRUCTURACION DE ARREGLOS --------------------------------
+// -------------------------------- IMPORTACIONES, EXPORTACION Y FUNCIONES DE ARREGLOS --------------------------------
+
+//importar
+import { heroes } from './data/heroes'; 
+
+//funcion que retorne la informacion de un heroe por id
+const getHeroe =  (id) => {
+    //la funcion find solo retorna un valor
+    return heroes.find((e) => e.id === id)
+}
+
+const getHeroeDC = (owner) => {
+    //la funcion filter retorna varios valores
+    return heroes.filter((e) => e.owner === owner);
+}
+
+console.log(getHeroe(2));
+console.log(getHeroeDC('Marvel'));
